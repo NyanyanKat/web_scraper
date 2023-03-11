9972b153ee0275db3c61768d7d04cb8d7bb0ed0c
Jimmy Chan <jchan85@gatech.edu>
2023-03-11 00:31:19 -0800

    initial layout

---
+++ README.md
@@ -0,0 +1,2 @@
+npx create-next-app -e with-tailwindcss ./
+code .
---
+++ components/Sidebar.tsx
@@ -0,0 +1,15 @@
+import { DocumentMagnifyingGlassIcon } from '@heroicons/react/24/outline';
+
+function Sidebar() {
+  return (
+    <div>
+      <div className="">
+        <DocumentMagnifyingGlassIcon className="h-16 md:w-16 text-indigo-600" />
+      </div>
+
+      <ul></ul>
+    </div>
+  );
+}
+
+export default Sidebar;
